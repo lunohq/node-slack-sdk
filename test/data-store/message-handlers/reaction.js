77b420a47999298075c836c24fc24e141a4e678a
Michael Hahn <mwhahn@gmail.com>
2016-08-21 21:16:04 -0700

    Add support for async data stores
    This changes the signature of data store methods to always return a
    promise. This makes it easier for us to drop in async data stores
    instead of the memory data store.

--- test/data-store/message-handlers/reaction.js
+++ test/data-store/message-handlers/reaction.js
@@ -9,26 +9,43 @@ var GENERAL_CHANNEL_ID = 'C0CHZA86Q';
 
 
 describe('RTM API Message Handlers: Reaction Events', function () {
+  var dataStore;
+
+  beforeEach(function () {
+    return getMemoryDataStore()
+      .then(function (store) {
+        dataStore = store;
+      });
+  });
 
   it('should add a reaction when a `reaction_added` event is received', function () {
-    var dataStore = getMemoryDataStore();
-    var channel = dataStore.getChannelById(GENERAL_CHANNEL_ID);
-    var message = channel.getMessageByTs('1444959632.000002');
-
-    messageHandlers.reaction_added(dataStore, getRTMMessageFixture('reaction_added'));
-    expect(message.reactions).to.have.length(1);
-    expect(message.reactions[0]).to.have.property('name', '+1');
+    return dataStore.getChannelById(GENERAL_CHANNEL_ID)
+      .then(function (channel) {
+        var message = channel.getMessageByTs('1444959632.000002');
+        return messageHandlers.reaction_added(dataStore, getRTMMessageFixture('reaction_added'))
+          .then(function () {
+            expect(message.reactions).to.have.length(1);
+            expect(message.reactions[0]).to.have.property('name', '+1');
+          });
+      });
   });
 
   it('should remove a reaction when a `reaction_removed` event is received', function () {
-    var dataStore = getMemoryDataStore();
-    var channel = dataStore.getChannelById(GENERAL_CHANNEL_ID);
-    var message = channel.getMessageByTs('1444959632.000002');
-
-    messageHandlers.reaction_added(dataStore, getRTMMessageFixture('reaction_added'));
-    expect(message.reactions[0]).to.have.property('name', '+1');
-    messageHandlers.reaction_removed(dataStore, getRTMMessageFixture('reaction_removed'));
-    expect(message.reactions).to.have.length(0);
+    return dataStore.getChannelById(GENERAL_CHANNEL_ID)
+      .then(function (channel) {
+        var message = channel.getMessageByTs('1444959632.000002');
+        return messageHandlers.reaction_added(dataStore, getRTMMessageFixture('reaction_added'))
+          .then(function () {
+            expect(message.reactions[0]).to.have.property('name', '+1');
+            return messageHandlers.reaction_removed(
+              dataStore,
+              getRTMMessageFixture('reaction_removed')
+            )
+              .then(function () {
+                expect(message.reactions).to.have.length(0);
+              });
+          });
+      });
   });
 
 });
--- test/data-store/message-handlers/team.js
+++ test/data-store/message-handlers/team.js
@@ -8,48 +8,62 @@ var getRTMMessageFixture = require('../../fixtures').getRTMMessage;
 
 
 describe('RTM API Message Handlers: Team Events', function () {
+  var dataStore;
+
+  beforeEach(function () {
+    return getMemoryDataStore()
+      .then(function (store) {
+        dataStore = store;
+      });
+  });
 
   it('updates the team domain when a `team_domain_change` message is received', function () {
-    var dataStore = getMemoryDataStore();
-    var team;
-
-    messageHandlers.team_domain_change(
-      '', 'T0CHZBU59', dataStore, getRTMMessageFixture('team_domain_change'));
-    team = dataStore.getTeamById('T0CHZBU59');
-
-    expect(team.url).to.equal('https://sslack-api-test.slack.com');
-    expect(team.domain).to.equal('sslack-api-test');
+    return messageHandlers.team_domain_change(
+      '', 'T0CHZBU59', dataStore, getRTMMessageFixture('team_domain_change'))
+      .then(function () {
+        return dataStore.getTeamById('T0CHZBU59');
+      })
+      .then(function (team) {
+        expect(team.url).to.equal('https://sslack-api-test.slack.com');
+        expect(team.domain).to.equal('sslack-api-test');
+      });
   });
 
   it('updates the team name when a `team_rename` message is received', function () {
-    var dataStore = getMemoryDataStore();
-    var team;
-
-    messageHandlers.team_rename('', 'T0CHZBU59', dataStore, getRTMMessageFixture('team_rename'));
-    team = dataStore.getTeamById('T0CHZBU59');
-
-    expect(team.name).to.equal('slack-api-test-test');
+    return messageHandlers.team_rename(
+      '',
+      'T0CHZBU59',
+      dataStore,
+      getRTMMessageFixture('team_rename')
+    )
+      .then(function () {
+        return dataStore.getTeamById('T0CHZBU59');
+      })
+      .then(function (team) {
+        expect(team.name).to.equal('slack-api-test-test');
+      });
   });
 
   it('updates a team preference when a `team_pref_change` message is received', function () {
-    var dataStore = getMemoryDataStore();
     var prefChangeMsg = getRTMMessageFixture('team_pref_change');
-    var team;
-
-    messageHandlers.team_pref_change('', 'T0CHZBU59', dataStore, prefChangeMsg);
-    team = dataStore.getTeamById('T0CHZBU59');
-    expect(team.prefs[prefChangeMsg.name]).to.equal(prefChangeMsg.value);
+    return messageHandlers.team_pref_change('', 'T0CHZBU59', dataStore, prefChangeMsg)
+      .then(function () {
+        return dataStore.getTeamById('T0CHZBU59');
+      })
+      .then(function (team) {
+        expect(team.prefs[prefChangeMsg.name]).to.equal(prefChangeMsg.value);
+      });
   });
 
   it('adds a new user to a team when a `team_join` message is received', function () {
-    var dataStore = getMemoryDataStore();
     var teamJoinMsg = getRTMMessageFixture('team_join');
-    var user;
-
-    messageHandlers.team_join(dataStore, teamJoinMsg);
-    user = dataStore.getUserById('U0EV582MU');
-
-    expect(user).to.be.an.instanceof(models.User);
+    return messageHandlers.team_join(dataStore, teamJoinMsg)
+      .then(function () {
+        return dataStore.getUserById('U0EV582MU');
+      })
+      .then(function (user) {
+        expect(user).to.be.an.instanceof(models.User);
+      });
   });
 
 });
